f99fff785936f9349f343194382046b178054584
Danielle <danielle.sackstein94@gmail.com>
2017-10-28 19:31:29 +0300

    Changed constant

--- ChangeBase.c
+++ ChangeBase.c
@@ -2,8 +2,14 @@
 #include <string.h>
 #include <stdlib.h>
 
-#define OK                  0
-#define NOT_A_NUMBER        1
+#define OK                  		0
+#define NOT_A_NUMBER        		1
+#define FAILED_TO_READ_INPUT        2
+#define TOO_FEW_ARGS_IN_INPUT       3
+#define EXPECTED_ARG_COUNT			4
+
+
+#define MAX_DIGITS_IN_ORIGINAL_NUMBER 	6
 
 struct Input
 {
@@ -14,7 +20,7 @@ struct Input
 struct Output
 {
 	int startIndex;
-	int result[6];
+	int result[MAX_DIGITS_IN_ORIGINAL_NUMBER];
 };
 
 struct Input gInput;
@@ -46,22 +52,22 @@ int parseInteger(char *str, int length, int base, int *pResult)
 
 int changeBase(int value, int base, int result[6], int *pStartIndex)
 {
-	for (int i = 0; i < 6; i++) {
+	for (int i = 0; i < MAX_DIGITS_IN_ORIGINAL_NUMBER; i++) {
 		result[i] = 0;
 	}
-	*pStartIndex = 5;
+	*pStartIndex = MAX_DIGITS_IN_ORIGINAL_NUMBER-1;
 
 	int smallestPowerAbove = 1;
 	int power = 0;
 
-	for (power = 0; power < 6; power++) {
+	for (power = 0; power < MAX_DIGITS_IN_ORIGINAL_NUMBER; power++) {
 		if (smallestPowerAbove > value) {
 			break;
 		}
 		smallestPowerAbove *= base;
 	}
 
-	if (power == 7) {
+	if (power == MAX_DIGITS_IN_ORIGINAL_NUMBER+1) {
 		return -1;
 	}
 
@@ -73,10 +79,10 @@ int changeBase(int value, int base, int result[6], int *pStartIndex)
 
 	int smallestPowerBelow = smallestPowerAbove / base;
 	int remain = value;
-	int place = 5 - power;
+	int place = MAX_DIGITS_IN_ORIGINAL_NUMBER-1 - power;
 	*pStartIndex = place;
 
-	while (place < 6) {
+	while (place < MAX_DIGITS_IN_ORIGINAL_NUMBER) {
 		int digit = remain / smallestPowerBelow;
 		result[place] = digit;
 
@@ -105,26 +111,88 @@ int getIntegerArgument(char *str, int base, int *pResult)
 	return EXIT_SUCCESS;
 }
 
-int readInput(int argc, char *argv[])
+int readArguments(char **argv)
 {
 	int originalBase = 0;
 
-	int error = getIntegerArgument(argv[1], 10, &originalBase);
+	int error = getIntegerArgument(argv[0], 10, &originalBase);
 	if (error != 0) {
 		return error;
 	}
 
-	error = getIntegerArgument(argv[2], 10, &gInput.newBase);
+	error = getIntegerArgument(argv[1], 10, &gInput.newBase);
 	if (error != 0) {
 		return error;
 	}
 
-	error = getIntegerArgument(argv[3], originalBase, &gInput.originalValue);
+	error = getIntegerArgument(argv[2], originalBase, &gInput.originalValue);
 	if (error != 0) {
 		return error;
 	}
 
 	return 0;
+}
+
+int parseInputString(char* inputString, char* argv[EXPECTED_ARG_COUNT])
+{
+	argv[0] = inputString;
+
+	char* ptr = inputString;	int argIndex = 1;
+
+	while (*ptr != 0)
+	{
+		if (*ptr == ' ')
+		{
+			*ptr = 0; // mark the end of the previous argument with null
+
+			// this argument should point to the char after the space
+
+			argv[argIndex] = ptr + 1;
+			argIndex++;
+			if (argIndex == EXPECTED_ARG_COUNT)
+			{
+				break;
+			}
+		}
+		ptr++;
+	}
+
+	if (argIndex < EXPECTED_ARG_COUNT){
+		return TOO_FEW_ARGS_IN_INPUT;
+	}
+
+	return OK;
+}
+
+int readInput()
+{
+	// see https://linux.die.net/man//3/getline for the usage of getline()
+
+	char *inputString = 0;
+	size_t size = 0;
+
+	ssize_t length = getline(&inputString, &size, stdin);
+	if (length == -1)
+	{
+		return FAILED_TO_READ_INPUT;
+	}
+
+	// Replace the terminating '\n' with a null to make this a kosher string
+
+	inputString[length-1] = 0;
+
+	char* argv[EXPECTED_ARG_COUNT];
+	int error = parseInputString(inputString, argv);
+	if (error != OK)
+	{
+		return error;
+	}
+
+	readArguments(argv);
+
+	free(inputString);
+
+	return OK;
 }
 
 void printOutput(int startIndex, int *result)
@@ -176,7 +244,8 @@ int Test()
 
 int Run(int argc, char *argv[])
 {
-	int error = readInput(argc, argv);
+//	int error = readArguments(argv + 1);
+	int error = readInput();
 	if (error != 0) {
 		return error;
 	}
