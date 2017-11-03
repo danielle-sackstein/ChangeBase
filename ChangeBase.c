d87630fd64d027f650899c46cbcddfc0040c72b9
David <davids@codeprecise.com>
2017-11-03 22:49:02 +0200

    Fixed dangling pointer Fixed length of result array

--- ChangeBase.c
+++ ChangeBase.c
@@ -8,29 +8,21 @@
 #define NOT_A_NUMBER                    1
 #define FAILED_TO_READ_INPUT            2
 
-
 #define EXPECTED_ARG_COUNT              3
 #define MAX_DIGITS_IN_ORIGINAL_NUMBER   6
+
+/*
+ * The maximum number of digits required for the new representation is 20
+ * because the original number is max 999999 and the minimum newbase is 2
+ * and 999999 in base 2 requires 20 digits
+ * (1024 * 1024 is equal to 20 1's in base 2)
+*/
+#define MAX_DIGITS_IN_NEW_NUMBER   		20
 #define MAX_DIGITS_IN_BASE              2
-#define MAX_CHARS_IN_INPUT            \
-            MAX_DIGITS_IN_BASE + 1 + \
-            MAX_DIGITS_IN_BASE + 1 + \
-            MAX_DIGITS_IN_ORIGINAL_NUMBER + 1
-
-struct Input
-{
-	int newBase;
-	int originalValue;
-};
-
-struct Output
-{
-	int startIndex;
-	long double result[MAX_DIGITS_IN_ORIGINAL_NUMBER];
-};
-
-struct Input gInput;
-struct Output gOutput;
+#define MAX_CHARS_IN_INPUT                   \
+			(MAX_DIGITS_IN_BASE + 1 +        \
+            MAX_DIGITS_IN_BASE + 1 +         \
+            MAX_DIGITS_IN_ORIGINAL_NUMBER + 1)
 
 int parseInteger(char *str, int base, int *pResult)
 {
@@ -62,9 +54,13 @@ int parseInteger(char *str, int base, int *pResult)
 // 14 / 4  : 3   2
 // 3 / 4   : 0   3
 
-void getDigitsInNewBase(int value, int base, long double *result, int *pStartIndex)
+void getDigitsInNewBase(
+	int value,
+	int base,
+	int result[MAX_DIGITS_IN_NEW_NUMBER],
+	int *pStartIndex)
 {
-	int startIndex = MAX_DIGITS_IN_ORIGINAL_NUMBER;
+	int startIndex = MAX_DIGITS_IN_NEW_NUMBER;
 
 	for (int ratio = value; ratio > 0; startIndex--) {
 		result[startIndex - 1] = ratio % base;
@@ -74,7 +70,10 @@ void getDigitsInNewBase(int value, int base, long double *result, int *pStartInd
 	*pStartIndex = startIndex;
 }
 
-int parseArguments(char **argv)
+int parseArguments(
+	char *argv[EXPECTED_ARG_COUNT],
+	int* pNewBase,
+	int* pOriginalValue)
 {
 	int originalBase = 10;
 
@@ -83,12 +82,12 @@ int parseArguments(char **argv)
 		return error;
 	}
 
-	error = parseInteger(argv[1], 10, &gInput.newBase);
+	error = parseInteger(argv[1], 10, pNewBase);
 	if (error != OK) {
 		return error;
 	}
 
-	return parseInteger(argv[2], originalBase, &gInput.originalValue);
+	return parseInteger(argv[2], originalBase, pOriginalValue);
 }
 
 /**
@@ -96,7 +95,9 @@ int parseArguments(char **argv)
  * @param inputString  pointer to char
  * @param argv
  */
-void parseInputString(char *inputString, char *argv[EXPECTED_ARG_COUNT])
+void parseInputString(
+	char inputString[MAX_CHARS_IN_INPUT],
+	char *argv[EXPECTED_ARG_COUNT])
 {
 	char delimiters[] = " \n";
 
@@ -111,10 +112,10 @@ void parseInputString(char *inputString, char *argv[EXPECTED_ARG_COUNT])
  * @param argv a pointer to an array of chars
  * @return 0 if the program did not fail, and 1 otherwise.
  */
-int readInputArgs(char **argv)
+int readInputArgs(
+	char inputString[MAX_CHARS_IN_INPUT],
+	char *argv[EXPECTED_ARG_COUNT])
 {
-	char inputString[MAX_CHARS_IN_INPUT];
-
 	char *str = fgets(inputString, MAX_CHARS_IN_INPUT, stdin);
 	if (str == 0) {
 		return FAILED_TO_READ_INPUT;
@@ -125,10 +126,10 @@ int readInputArgs(char **argv)
 	return OK;
 }
 
-void printOutput(int startIndex, long double *result)
+void printOutput(int startIndex, int *result)
 {
-	for (int i = startIndex; i < 6; i++) {
-		printf("%llu", (unsigned long long)result[i]);
+	for (int i = startIndex; i < MAX_DIGITS_IN_NEW_NUMBER; i++) {
+		printf("%d", result[i]);
 	}
 	printf("\n");
 }
@@ -143,25 +144,32 @@ void printOutput(int startIndex, long double *result)
 int main(int argc, char *argv[])
 {
 	char *inputArgs[EXPECTED_ARG_COUNT];
+	char inputString[MAX_CHARS_IN_INPUT];
 
-	int error = readInputArgs(inputArgs);
+	int error = readInputArgs(inputString, inputArgs);
 	if (error != OK) {
 		printf("Failed to read input");
 		return EXIT_FAILURE;
 	}
 
-	error = parseArguments(inputArgs);
+	int newBase;
+	int originalValue;
+
+	error = parseArguments(inputArgs, &newBase, &originalValue);
 	if (error != OK) {
 		fprintf(stderr, "invalid!!\n");
 		return EXIT_FAILURE;
 	}
 
-	getDigitsInNewBase(gInput.originalValue,
-					   gInput.newBase,
-					   gOutput.result,
-					   &gOutput.startIndex);
+	int startIndex;
+	int result[MAX_DIGITS_IN_NEW_NUMBER];
 
-	printOutput(gOutput.startIndex, gOutput.result);
+	getDigitsInNewBase(originalValue,
+					   newBase,
+					   result,
+					   &startIndex);
+
+	printOutput(startIndex, result);
 
 	return EXIT_SUCCESS;
 }
